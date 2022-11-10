227d165ae75f0d13a873b473c1da7225ee62129b
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-10 11:32:52 +0100

    setup validation

--- 11_10_mongoose_utils/routers/usersRouter.js
+++ 11_10_mongoose_utils/routers/usersRouter.js
@@ -2,13 +2,14 @@ import express from 'express'
 import * as controller from '../controllers/usersController.js'
 import auth from '../lib/middlewares/auth.js'
 import 'express-async-errors'
+import * as validations from '../lib/validations/userValidations.js'
 
 const app = express.Router()
 
 app.get('/', auth, controller.getUser)
-app.post('/register', controller.createUser)
-app.post('/login', controller.login)
-app.patch('/', auth, controller.updateUser)
+app.post('/register', ...validations.register, controller.createUser)
+app.post('/login', ...validations.login, controller.login)
+app.patch('/', auth, ...validations.updateUser, controller.updateUser)
 
 
 export default app
