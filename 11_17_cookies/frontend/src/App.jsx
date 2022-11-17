4c24f23acf6d21daaf393a557aef565d8df31d08
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-17 10:20:17 +0100

    created register form

--- 11_17_cookies/frontend/src/App.jsx
+++ 11_17_cookies/frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Register from './components/Register'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='App'>
+      <Register />
+    </div>
   )
 }
 
---
+++ 11_17_cookies/frontend/src/components/Register.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import './Register.css'
+
+export default function Register () {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
+  const submit = (e) => {
+    e.preventDefault()
+    console.log('submit', {name, email, password})
+  }
+
+  return (
+    <form className='Register' onSubmit={submit}>
+      <h3>Registrieren</h3>
+      <input type='text' placeholder='Name...' value={name} onChange={e => setName(e.target.value)}/>
+      <input type='email' placeholder='email...' value={email} onChange={e => setEmail(e.target.value)}/>
+      <input type='password' placeholder='Password...' value={password} onChange={e => setPassword(e.target.value)}/>
+      <button type='submit'>Absenden</button>
+    </form>
+  )
+}
