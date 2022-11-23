6ab071d79ec0a33d77c2feadd18a46499151b760
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-23 14:47:41 +0100

    created ts-demo script

---
+++ 11_23_typescript/index.ts
@@ -0,0 +1,90 @@
+// number -> number
+// symbol -> symbol
+// string -> string
+// boolean -> boolean
+// null -> null
+// undefined -> undefined
+// object/array
+//  -> any
+
+/** simple types */
+// let myNumber:number
+
+// myNumber = 4
+// myNumber = 'Hello World'
+
+// console.log(myNumber)
+
+let myVar:number|string = 3
+
+myVar = ''
+
+
+/** object */
+
+type User = { 
+  name: string
+  age: number
+}
+
+type Account = {
+  user: null | undefined | User
+  value: string
+  timestamp?: number
+  friends: User[]
+}
+
+let obj1:Account = {
+  user: {
+    name: 'Manu',
+    age: 10,
+  },
+  value: '',
+  timestamp: 0,
+  friends: [
+    {
+      name: 'Name1',
+      age: 20
+    },
+    {
+      name: 'Name2',
+      age: 22
+    },
+  ],
+}
+
+let obj2:Account = {
+  user: null,
+  value: '3',
+  friends: [],
+}
+
+type LogUserReturn = null | User
+
+
+function logUser (obj:Account) {
+  if(obj.user) {
+    const user = obj.user
+    console.log(user.name)
+    return obj.user
+  }
+  else {
+    const user = obj.user
+    console.log('no user found')
+    return null
+  }
+}
+
+const result = logUser(obj2)
+const timestamp = obj1.timestamp
+const myName = obj1.user?.name
+
+if(result) {
+  console.log(result.name)
+}
+
+
+for(const friend of obj1.friends) {
+  console.log(friend.name)
+}
+
---
+++ 11_23_typescript/test.js
@@ -0,0 +1,5 @@
+let myNumber = 3
+
+myNumber = 'Hello World'
+
+console.log(myNumber)
