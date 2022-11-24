2dcf70a3482d5a79d6c9a8b936542c3913c696c4
manuelJung <manuel.jung.wwi12@gmail.com>
2022-11-24 11:13:36 +0100

    created forms

--- 11_24_react-hooks/frontend/src/routes/Login.tsx
+++ 11_24_react-hooks/frontend/src/routes/Login.tsx
@@ -1,6 +1,19 @@
+import './Login.css'
 
 export default function Login () {
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-    <h1>Login</h1>
+    <form className='Login' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label htmlFor='email'>Email</label>
+      <input id='email' type='email' placeholder="Email..."/>
+      <label htmlFor='password'>Passwort</label>
+      <input id='password' type='password' placeholder="Password..."/>
+      <button>Send</button>
+    </form>
   )
 }
--- 11_24_react-hooks/frontend/src/routes/Register.tsx
+++ 11_24_react-hooks/frontend/src/routes/Register.tsx
@@ -1,6 +1,19 @@
+import './Register.css'
 
 export default function Register () {
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-    <h1>Register</h1>
+    <form className='Register' onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <label htmlFor='email'>Email</label>
+      <input id='email' type='email' placeholder="Email..."/>
+      <label htmlFor='password'>Passwort</label>
+      <input id='password' type='password' placeholder="Password..."/>
+      <button>Send</button>
+    </form>
   )
 }
